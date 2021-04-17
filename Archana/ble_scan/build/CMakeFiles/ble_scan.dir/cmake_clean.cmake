96b075d902d2d7e741a916d5be3443ee582043ce
Archana Kalathil Venu <archanakvenu@gmail.com>
2021-04-17 19:21:17 -0400

    added code running on Pi reading temp data

---
+++ Archana/ble_scan/build/CMakeFiles/ble_scan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ble_scan.dir/ble_scan.c.o"
+  "ble_scan"
+  "ble_scan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ble_scan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
