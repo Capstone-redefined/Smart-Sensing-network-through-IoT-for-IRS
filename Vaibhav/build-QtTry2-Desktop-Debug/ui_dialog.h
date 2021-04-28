99d224230c719ea41e4787cf7582df9999af6dff
ArchanaKalathil <archanakvenu@gmail.com>
2021-04-28 12:05:36 -0400

    added 2nd counter

--- Vaibhav/build-QtTry2-Desktop-Debug/ui_dialog.h
+++ Vaibhav/build-QtTry2-Desktop-Debug/ui_dialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'dialog.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.8
+** Created by: Qt User Interface Compiler version 5.11.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -43,24 +43,24 @@ public:
     void setupUi(QDialog *Dialog)
     {
         if (Dialog->objectName().isEmpty())
-            Dialog->setObjectName(QString::fromUtf8("Dialog"));
+            Dialog->setObjectName(QStringLiteral("Dialog"));
         Dialog->resize(443, 322);
         label = new QLabel(Dialog);
-        label->setObjectName(QString::fromUtf8("label"));
+        label->setObjectName(QStringLiteral("label"));
         label->setGeometry(QRect(400, 10, 41, 61));
         layoutWidget = new QWidget(Dialog);
-        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
         layoutWidget->setGeometry(QRect(4, 0, 391, 71));
         horizontalLayout = new QHBoxLayout(layoutWidget);
-        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
         horizontalLayout->setContentsMargins(0, 0, 0, 0);
         tempLabel = new QLabel(layoutWidget);
-        tempLabel->setObjectName(QString::fromUtf8("tempLabel"));
+        tempLabel->setObjectName(QStringLiteral("tempLabel"));
 
         horizontalLayout->addWidget(tempLabel);
 
         lcdNumberTemp = new QLCDNumber(layoutWidget);
-        lcdNumberTemp->setObjectName(QString::fromUtf8("lcdNumberTemp"));
+        lcdNumberTemp->setObjectName(QStringLiteral("lcdNumberTemp"));
         QPalette palette;
         QBrush brush(QColor(164, 0, 0, 255));
         brush.setStyle(Qt::SolidPattern);
@@ -75,45 +75,39 @@ public:
         palette.setBrush(QPalette::Active, QPalette::ButtonText, brush2);
         QBrush brush3(QColor(0, 0, 0, 128));
         brush3.setStyle(Qt::SolidPattern);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette.setBrush(QPalette::Active, QPalette::PlaceholderText, brush3);
-#endif
         palette.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
         palette.setBrush(QPalette::Inactive, QPalette::Light, brush1);
         palette.setBrush(QPalette::Inactive, QPalette::Dark, brush1);
         palette.setBrush(QPalette::Inactive, QPalette::Text, brush2);
         palette.setBrush(QPalette::Inactive, QPalette::ButtonText, brush2);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette.setBrush(QPalette::Inactive, QPalette::PlaceholderText, brush3);
-#endif
         palette.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
         palette.setBrush(QPalette::Disabled, QPalette::Light, brush1);
         palette.setBrush(QPalette::Disabled, QPalette::Dark, brush1);
         palette.setBrush(QPalette::Disabled, QPalette::Text, brush1);
         palette.setBrush(QPalette::Disabled, QPalette::ButtonText, brush1);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette.setBrush(QPalette::Disabled, QPalette::PlaceholderText, brush3);
-#endif
         lcdNumberTemp->setPalette(palette);
         lcdNumberTemp->setContextMenuPolicy(Qt::DefaultContextMenu);
         lcdNumberTemp->setDigitCount(10);
-        lcdNumberTemp->setProperty("value", QVariant(0.000000000000000));
+        lcdNumberTemp->setProperty("value", QVariant(0));
 
         horizontalLayout->addWidget(lcdNumberTemp);
 
         layoutWidget_2 = new QWidget(Dialog);
-        layoutWidget_2->setObjectName(QString::fromUtf8("layoutWidget_2"));
+        layoutWidget_2->setObjectName(QStringLiteral("layoutWidget_2"));
         layoutWidget_2->setGeometry(QRect(4, 80, 391, 71));
         horizontalLayout_3 = new QHBoxLayout(layoutWidget_2);
-        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
         horizontalLayout_3->setContentsMargins(0, 0, 0, 0);
         label_3 = new QLabel(layoutWidget_2);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setObjectName(QStringLiteral("label_3"));
 
         horizontalLayout_3->addWidget(label_3);
 
         lcdNumberPersons = new QLCDNumber(layoutWidget_2);
-        lcdNumberPersons->setObjectName(QString::fromUtf8("lcdNumberPersons"));
+        lcdNumberPersons->setObjectName(QStringLiteral("lcdNumberPersons"));
         QPalette palette1;
         palette1.setBrush(QPalette::Active, QPalette::WindowText, brush2);
         QBrush brush4(QColor(46, 52, 54, 255));
@@ -122,83 +116,78 @@ public:
         palette1.setBrush(QPalette::Active, QPalette::Dark, brush4);
         palette1.setBrush(QPalette::Active, QPalette::Text, brush2);
         palette1.setBrush(QPalette::Active, QPalette::ButtonText, brush2);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette1.setBrush(QPalette::Active, QPalette::PlaceholderText, brush3);
-#endif
         palette1.setBrush(QPalette::Inactive, QPalette::WindowText, brush2);
         palette1.setBrush(QPalette::Inactive, QPalette::Light, brush4);
         palette1.setBrush(QPalette::Inactive, QPalette::Dark, brush4);
         palette1.setBrush(QPalette::Inactive, QPalette::Text, brush2);
         palette1.setBrush(QPalette::Inactive, QPalette::ButtonText, brush2);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette1.setBrush(QPalette::Inactive, QPalette::PlaceholderText, brush3);
-#endif
         palette1.setBrush(QPalette::Disabled, QPalette::WindowText, brush4);
         palette1.setBrush(QPalette::Disabled, QPalette::Light, brush4);
         palette1.setBrush(QPalette::Disabled, QPalette::Dark, brush4);
         palette1.setBrush(QPalette::Disabled, QPalette::Text, brush4);
         palette1.setBrush(QPalette::Disabled, QPalette::ButtonText, brush4);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette1.setBrush(QPalette::Disabled, QPalette::PlaceholderText, brush3);
-#endif
         lcdNumberPersons->setPalette(palette1);
         lcdNumberPersons->setDigitCount(9);
 
         horizontalLayout_3->addWidget(lcdNumberPersons);
 
         layoutWidget_3 = new QWidget(Dialog);
-        layoutWidget_3->setObjectName(QString::fromUtf8("layoutWidget_3"));
+        layoutWidget_3->setObjectName(QStringLiteral("layoutWidget_3"));
         layoutWidget_3->setGeometry(QRect(4, 160, 391, 71));
         horizontalLayout_4 = new QHBoxLayout(layoutWidget_3);
-        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
         horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
         label_4 = new QLabel(layoutWidget_3);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setObjectName(QStringLiteral("label_4"));
 
         horizontalLayout_4->addWidget(label_4);
 
         lcdNumberHour = new QLCDNumber(layoutWidget_3);
-        lcdNumberHour->setObjectName(QString::fromUtf8("lcdNumberHour"));
+        lcdNumberHour->setObjectName(QStringLiteral("lcdNumberHour"));
         QPalette palette2;
         palette2.setBrush(QPalette::Active, QPalette::WindowText, brush2);
         palette2.setBrush(QPalette::Active, QPalette::Dark, brush2);
         palette2.setBrush(QPalette::Active, QPalette::Text, brush2);
         palette2.setBrush(QPalette::Active, QPalette::ButtonText, brush2);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette2.setBrush(QPalette::Active, QPalette::PlaceholderText, brush3);
-#endif
         palette2.setBrush(QPalette::Inactive, QPalette::WindowText, brush2);
         palette2.setBrush(QPalette::Inactive, QPalette::Dark, brush2);
         palette2.setBrush(QPalette::Inactive, QPalette::Text, brush2);
         palette2.setBrush(QPalette::Inactive, QPalette::ButtonText, brush2);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette2.setBrush(QPalette::Inactive, QPalette::PlaceholderText, brush3);
-#endif
         palette2.setBrush(QPalette::Disabled, QPalette::WindowText, brush2);
         palette2.setBrush(QPalette::Disabled, QPalette::Dark, brush2);
         palette2.setBrush(QPalette::Disabled, QPalette::Text, brush2);
         palette2.setBrush(QPalette::Disabled, QPalette::ButtonText, brush2);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette2.setBrush(QPalette::Disabled, QPalette::PlaceholderText, brush3);
-#endif
         lcdNumberHour->setPalette(palette2);
         lcdNumberHour->setDigitCount(9);
 
         horizontalLayout_4->addWidget(lcdNumberHour);
 
         widget = new QWidget(Dialog);
-        widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(0, 240, 391, 71));
+        widget->setObjectName(QStringLiteral("widget"));
+        widget->setGeometry(QRect(10, 240, 391, 71));
         horizontalLayout_2 = new QHBoxLayout(widget);
-        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
         horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
         label_2 = new QLabel(widget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setObjectName(QStringLiteral("label_2"));
 
         horizontalLayout_2->addWidget(label_2);
 
         label_5 = new QLabel(widget);
-        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setObjectName(QStringLiteral("label_5"));
+        QPalette palette3;
+        QBrush brush5(QColor(255, 255, 255, 255));
+        brush5.setStyle(Qt::SolidPattern);
+        palette3.setBrush(QPalette::Active, QPalette::Light, brush5);
+        palette3.setBrush(QPalette::Inactive, QPalette::Light, brush5);
+        palette3.setBrush(QPalette::Disabled, QPalette::Light, brush5);
+        label_5->setPalette(palette3);
 
         horizontalLayout_2->addWidget(label_5);
 
@@ -215,7 +204,7 @@ public:
         tempLabel->setText(QApplication::translate("Dialog", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600; color:#000000;\">Current Temperature</span></p></body></html>", nullptr));
         label_3->setText(QApplication::translate("Dialog", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Persons in the zone</span></p></body></html>", nullptr));
         label_4->setText(QApplication::translate("Dialog", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Today's Hours</span></p></body></html>", nullptr));
-        label_2->setText(QApplication::translate("Dialog", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Motor's condition</span></p></body></html>", nullptr));
+        label_2->setText(QApplication::translate("Dialog", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\"> Motor's condition</span></p></body></html>", nullptr));
         label_5->setText(QApplication::translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:16pt; font-weight:600; color:#4e9a06;\">GOOD</span></p></body></html>", nullptr));
     } // retranslateUi
 
