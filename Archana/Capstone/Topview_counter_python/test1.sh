7f33929ae916e3176dcc28f851616f14990306fa
ArchanaKalathil <archanakvenu@gmail.com>
2021-04-29 18:25:23 -0400

    ............Adding all data files to git..............

--- Archana/Capstone/Topview_counter_python/test1.sh
+++ Archana/Capstone/Topview_counter_python/test1.sh
@@ -10,6 +10,14 @@ wait
 ./run_put_object "T_hours.txt"
 ./run_put_object "N_people.txt"
 
+cp "temperature.txt" ~/Smart-Sensing-network-through-IoT-for-IRS/Vaibhav/build-QtTry2-Desktop-Debug/tempdata.txt
+cp "T_hours.txt" ~/Smart-Sensing-network-through-IoT-for-IRS/Vaibhav/build-QtTry2-Desktop-Debug/hours.txt
+cp "N_people.txt" ~/Smart-Sensing-network-through-IoT-for-IRS/Vaibhav/build-QtTry2-Desktop-Debug/personsdata.txt
 
+cd ~/Smart-Sensing-network-through-IoT-for-IRS
+git pull
+git add .
+git commit -m "............Adding all data files to git.............."
+git push
 
 exit 0
